17923badcee68de1e44b6d0078c187197ad4746e
Nitish <nitishkumaryaduvanshi1@gmail.com>
2024-03-29 16:59:28 +0530

    2nd commit after clone

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,7 +19,7 @@ export default function Header() {
       <header id="main-header">
         <div id="title">
           <img src={logoImg} alt="A restaurant " />
-          <h1>React Food</h1>
+          <h1>City Cafe</h1>
         </div>
         <nav>
           <Button textOnly onClick={handleShowCart}>
